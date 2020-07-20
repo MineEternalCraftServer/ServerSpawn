f68046762715b46e28f2a7ca417c9653e6efbf8d
Chiharu-Hagihara <chiharuhagihara803@gmail.com>
2020-07-20 21:28:31 +0900

    initial commit

---
+++ src/main/java/server/mecs/serverspawn/ServerSpawn.java
@@ -0,0 +1,17 @@
+package server.mecs.serverspawn;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ServerSpawn extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
