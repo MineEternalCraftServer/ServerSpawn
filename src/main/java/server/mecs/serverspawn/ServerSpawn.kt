b18ecaba1a208f720579115927714f118ab3f597
ubiquitaku <ubiquitaku@outlook.jp>
2020-07-26 11:07:46 +0900

    （ ＾ω＾）おっ

--- src/main/java/server/mecs/serverspawn/ServerSpawn.kt
+++ src/main/java/server/mecs/serverspawn/ServerSpawn.kt
@@ -3,7 +3,6 @@ package server.mecs.serverspawn
 import org.bukkit.Location
 import org.bukkit.command.Command
 import org.bukkit.command.CommandSender
-import org.bukkit.command.ConsoleCommandSender
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
@@ -22,15 +21,14 @@ class ServerSpawn : JavaPlugin(), Listener {
     override fun onEnable() {
         // Plugin startup logic
         saveDefaultConfig()
-        reloadConfig()
         x = config.getInt("x", 0)
         y = config.getInt("y", 0)
         z = config.getInt("z", 0)
-        w = config.getString("w", null)
+        w = config.getString("w", "world")
         s = config.getBoolean("s", false)
         saveConfig()
-        reloadConfig()
-        println("座標の情報が読み取れなかった場合に設定されます\non/offの情報が読み取れなかった場合falseに設定されます\nワールド名が読み取れなかった場合nullに設定されます")
+        l = Location(server.getWorld(w), x.toDouble(), y.toDouble(), z.toDouble())
+        println("座標の情報が読み取れなかった場合に0,70,0設定されます\non/offの情報が読み取れなかった場合falseに設定されます\nワールド名が読み取れなかった場合worldに設定されます")
         getCommand("sspawn").executor = this
         server.pluginManager.registerEvents(this, this)
     }
@@ -41,75 +39,68 @@ class ServerSpawn : JavaPlugin(), Listener {
 
     override fun onCommand(sender: CommandSender, command: Command, label: String?, args: Array<String>): Boolean {
         if (command.name == "sspawn") {
-
-            val p = sender as Player
-
-            if (sender is ConsoleCommandSender) {
-                sender.sendMessage("This command cannot be executed from the console.")
-                return false
+            if (!sender.isOp) {
+                sender.sendMessage("§c§lあなたにはこのこまんどを実行する権限がありません")
+                return true
             }
-
-            if (args[0] == "set") {
-                if (p.hasPermission("sspawn.admin")) {
-                    config!!["x"] = p.location.blockX
-                    config!!["y"] = p.location.blockY
-                    config!!["z"] = p.location.blockZ
-                    config!!["w"] = p.location.world
-                    saveConfig()
-                    reloadConfig()
-                    sender.sendMessage("座標を保存しました")
-                    return true
-                } else {
-                    p.sendMessage("§cThis command can only be executed by a player with <sspawn.admin> privileges.")
-                }
-            }
-
             if (args.size != 1) {
-                p.sendMessage("/sspawn set : 実行しているプレイヤーが立っている場所をスポーン地点に設定します")
-                p.sendMessage("/sspawn <x> <y> <z> : プレイヤーがログイン時にスポーンする座標を設定します")
-                p.sendMessage("/sspawn reload : 設定値をリロードします")
+                sender.sendMessage("/sspawn set : 実行しているプレイヤーが立っている場所をスポーン地点に設定します(consoleからの実行は不可能)")
+                sender.sendMessage("/sspawn <x> <y> <z> : プレイヤーがログイン時にスポーンする座標を設定します")
+                sender.sendMessage("/sspawn reload : 設定値をリロードします")
                 return true
             }
             if (args[0] == "on") {
-                if (p.hasPermission("sspawn.admin")) {
-                    config!!["s"] = true
+                if (!(s)) {
+                    config["s"] = true
                     saveConfig()
-                    reloadConfig()
+                    s = true
                     sender.sendMessage("serverspawnを有効化しました")
-                } else {
-                    p.sendMessage("§cThis command can only be executed by a player with <sspawn.admin> privileges.")
+                    return true
                 }
+                sender.sendMessage("onになっています")
                 return true
             }
             if (args[0] == "off") {
-                if (p.hasPermission("sspawn.admin")) {
-                    config!!["s"] = false
+                if (s) {
+                    config["s"] = false
                     saveConfig()
-                    reloadConfig()
+                    s = false
                     sender.sendMessage("serverspawnを無効化しました")
-                } else {
-                    p.sendMessage("§cThis command can only be executed by a player with <sspawn.admin> privileges.")
+                    return true
                 }
+                sender.sendMessage("offになっています")
                 return true
             }
             if (args[0] == "reload") {
-                if (p.hasPermission("sspawn.admin")) {
-                    reloadConfig()
-                } else {
-                    p.sendMessage("§cThis command can only be executed by a player with <sspawn.admin> privileges.")
-                }
+                reloadConfig()
+                sender.sendMessage("リロード完了")
                 return true
             }
+            if (args[0] == "set") {
+                if (sender is Player) {
+                    val p = sender
+                    config["x"] = p.location.x
+                    config["y"] = p.location.y
+                    config["z"] = p.location.z
+                    config["w"] = p.location.world.name
+                    saveConfig()
+                    sender.sendMessage("座標を保存しました")
+                    return true
+                }
+                sender.sendMessage("consoleからの実行はできません")
+                return true
+            }
+            return true
         }
         return true
     }
 
     @EventHandler
     fun spawn(e: PlayerJoinEvent) {
-        if (!s) {
+        if (s) {
+            val p = e.player
+            p.teleport(l)
             return
         }
-        l = Location(server.getWorld(w), x.toDouble(), y.toDouble(), z.toDouble())
-        e.player.teleport(l)
     }
 }
